f6264042b950d59c069328e192614d276b9faaeb
Ku Chen-Hao <maple52046@gmail.com>
2018-01-09 15:07:37 +0800

    Integrate basic-select component to TWSE backtest

--- src/components/backtest.vue
+++ src/components/backtest.vue
@@ -2,12 +2,18 @@
   <div class="date-time-picker">
     <h3>Select Date Range</h3>
     <DateTimePicker v-on:dateRangePicked="updateDateRange" />
-    <div>You can visit: <a v-bind:href="dataURL">api</a> to get data</div>
+    <h3>Select Item</h3>
+    <p>First stock: <SelectBasic v-on:optionPicked="updateSelectedStocks" v-bind:options="firstStocks" /></p>
+    <p>Second stock: <SelectBasic v-on:optionPicked="updateSelectedStocks" v-bind:options="secondStocks" /></p>
+    <p v-model=pairs>Selected stocks: {{ selectedPair }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import DateTimePicker from './el/DateTimePicker'
+import SelectBasic from './el/SelectBasic'
 
 export default {
   name: 'backtest',
@@ -15,18 +21,45 @@ export default {
     return {
       start: '',
       end: '',
-      dataURL: ''
+      firstStocks: [],
+      secondStocks: [],
+      pairs: null,
+      selectedPair: []
     }
   },
   components: {
-    DateTimePicker    
+    DateTimePicker,
+    SelectBasic
   },
   methods:{
     updateDateRange: function(dateRange){
       this.start = dateRange[0]
       this.end = dateRange[1]
-      this.dataURL = "/api/twse/backtest/pair?start=" + this.start + "&end=" + this.end
+      this.firstStocks = new Array()
+      this.pairs = new Array()
+
+      var api = "/api/twse/backtest/pair?start=" + this.start + "&end=" + this.end
+      axios.get(api).then(response => {
+        this.pairs = response.data[0]
+        Object.keys(this.pairs).sort().forEach(stock => {
+          this.firstStocks.push({'value': stock, 'label': stock})
+	      })
+      }).catch(e => {
+        console.log(e)
+      })
+    },
+    updateSelectedStocks: function(channel){
+      if (this.selectedPair.length < 2){
+        this.selectedPair.push(channel)
+        if (this.selectedPair.length == 1){
+          Object.keys(this.pairs[channel]).sort().forEach(stock => {
+            this.secondStocks.push({'value': stock, 'label': stock})
+          })
+        }
+      }
     }
   }
 }
 </script>
+
+<!-- vim: ts=2 sw=2 expandtab
---
+++ src/components/el/SelectBasic.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-select v-model="value" placeholder="Select"
+    @pick="(value) => pickOption(value)"
+    @change="(value) => pickOption(value)">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+  export default {
+    props: ['options'],
+    data() {
+      return {
+        value: ''
+      }
+    },
+    methods: {
+      pickOption: function (data){
+        this.$emit('optionPicked', data)
+      }
+    }
+  }
+</script>
+
+<!-- vim: ts=2 sw=2 expandtab
